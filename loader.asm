b8c7666e1a8729a7e683d9e30af555bf5aebace5
Davi Campos Ribeiro <dcr23@inf.ufpr.br>
2025-08-05 11:20:40 -0300

    add check for long mode support

--- loader.asm
+++ loader.asm
@@ -12,6 +12,11 @@ org 0x8000
 %define FILL_COLOR 0x01             ; blue on black
 %define PRINT_COLOR 0x07            ; grey on black
 
+%define EFLAGS_CPUID 0x200000
+%define CPUID_EXT 0x80000000
+%define CPUID_EXT_FEATURES 0x80000001
+%define CPUID_LONG_MODE_FLAG 0x20000000 
+
 ; entry point -----------------------------------------------------------------
 
 ; initialize segment registers
@@ -38,7 +43,56 @@ mov ax, (FILL_COLOR << 8) | FILL_CHAR
 
 rep stosw                   ; fill cx words at es:di with ax
 
+
+; check long mode support -----------------------------------------------------
+
+pushfd
+pop eax                     ; get eflags
+
+mov ecx, eax
+xor eax, EFLAGS_CPUID       ; flip 21th bit (id flag)
+
+push eax
+popfd                       ; commits changes to the eflags
+
+pushfd
+pop eax                     ; retrieve eflags to check if changes persisted
+
+push ecx
+popfd                       ; restore eflags
+
+xor eax, ecx
+jz err_nocpuid
+
+mov eax, CPUID_EXT
+cpuid                       ; queries max leaf value
+
+cmp eax, CPUID_EXT_FEATURES
+jb err_nocpuidext           ; check if it has required leaf level
+
+mov eax, CPUID_EXT_FEATURES
+cpuid                       ; query extended processor info
+ 
+test edx, CPUID_LONG_MODE_FLAG
+jz err_nolongmode
+
+
 mov si, str.hello
+
+; errors -----------------------------------------------------------------
+
+err_nocpuid:
+mov si, str.nocpuid
+jmp printerr
+
+err_nocpuidext:
+mov si, str.nocpuidext
+jmp printerr
+
+err_nolongmode:
+mov si, str.nolongmode
+jmp printerr
+
 
 ; print the error message string in si and halt
 printerr:
@@ -69,6 +123,17 @@ jmp halt
 str:
 .hello:
     db "hello cruel world", 0
+
+.nocpuid:
+    db "cpuid isnt available", 0
+
+.nocpuidext:
+    db "cpuid required extensions arent available", 0
+
+.nolongmode:
+    db "long mode not available", 0
+
+
 
 times 510 - ($ - $$) db 0   ; fill remaining bytes with zeroes
 dw 0xaa55                   ; mbr magic byte
