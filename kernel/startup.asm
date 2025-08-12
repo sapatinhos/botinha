0abb9b35c3bfc8d9c88218211dcbfed90faedd1d
phfr24 <phfr24@inf.ufpr.br>
2025-08-12 10:38:06 -0300

    blink blink

--- kernel/startup.asm
+++ kernel/startup.asm
@@ -87,6 +87,23 @@ mov ax, (FILL_COLOR << 8) | FILL_CHAR
 
 rep stosw                   ; fill cx words at es:di with ax
 
+; Enable blink
+; Read I/O Address 0x03DA to reset index/data flip-flop
+mov dx, 0x03DA
+in al, dx
+; Write index 0x30 to 0x03C0 to set register index to 0x30
+mov dx, 0x03C0
+mov al, 0x30
+out dx, al
+; Read from 0x03C1 to get register contents
+inc dx
+in al, dx
+; Set Bit 3 to enable Blink
+or al, 0x08
+; Write to 0x03C0 to update register with changed value
+dec dx
+out dx, al
+
 ; enter protected mode ------------------------------------------------------
 
 ; disable maskable interrupts
@@ -223,7 +240,6 @@ mov rax, (FILL_COLOR << 8) | FILL_CHAR
 mov rcx, VGA_TEXT_BUFFER_SIZE / 8
 rep stosq
 jmp kmain
-hlt
 
 ; 16 bits functions -------------------------------------------------------------------
 bits 16
